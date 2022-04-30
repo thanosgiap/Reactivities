25777b4caebd288703242046244b85fae074d99f
Thanos <thanosgiap@gmail.com>
2022-04-30 15:12:26 +0300

    Reactivities - Init Commit

---
+++ Domain/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Authentication;
+
+namespace Domain;
+using System;
+
+public class Activity
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Description { get; set; }
+
+    public string Category { get; set; }
+
+    public string City { get; set; }
+
+    public string Venue { get; set; }
+}
