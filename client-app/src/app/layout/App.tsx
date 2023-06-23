c30bdfca80472b9fc57473774de9eb83bdc5a81f
Thanos <thanosgiap@gmail.com>
2023-06-23 14:23:02 +0300

    Added styling using semanticUI components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,8 @@
 import { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Outlet, Route, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 
 function App() {
